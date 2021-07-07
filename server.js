7d8bf4369fa1d0ef11f5e2d807bc95957acfad62
Shivvers <ashivvers95@gmail.com>
2021-07-07 11:53:20 -0700

    server now operational but pathing to html routes has to be commented out, bummer

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { notes } = require('./db/db.json');
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/apiRoutes/htmlRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+//app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
